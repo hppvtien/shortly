d606da70ab14e35d0ccd1cbdc5c6a95a7f518c75
dainx217 <nguyenxuandai217@gmail.com>
2022-01-07 14:58:54 +0700

    add

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,33 +13,7 @@
     <h1>Test affilia444te{{ env('API_URL'), 'test' }}</h1>
 	<input type="button" name="myBtn" onClick="GetLocalIPAddress()" value="GetIp">
 </body>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
-<script type="text/javascript">
-    $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
-</script>
-<script>
-    var url_string = window.location.href;
-	var url = new URL(url_string);
-	var asmid = url.searchParams.get("asmid");
-	if(asmid != null){
-		var formData = { asmid: asmid };
-		if(sessionStorage.getItem('key_check') != asmid){
-			$.ajax({
-			type: "POST",
-			url: "http://127.0.0.1:8000/page_view",
-			data: formData,
-			dataType: "json",
-			encode: true,
-			}).done(function (data) {
-			sessionStorage.setItem('key_check', data.asmid);
-			});
-		}
-	}
-</script>
+
 
 </html>
